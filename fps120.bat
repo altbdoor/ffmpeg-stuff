7ab170dc4bc0719ddee4bbfc10792e3fe8f4f394
altbdoor <lancersupraskyline@gmail.com>
2019-11-24 22:44:27 +0800

    add fps minterpolate script

---
+++ fps120.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal enableExtensions disableDelayedExpansion
+
+echo Press 1 for 60fps, and 2 for 120fps
+set question="Enter mode: "
+set /p mode=%question%
+
+if exist .\bin\ffmpeg.exe (
+    if %mode% == 1 (
+        .\bin\ffmpeg.exe -y -hide_banner -i "%~1" -filter "minterpolate='fps=60'" "%~n1_60fps%~x1"
+    ) else if %mode% == 2 (
+        .\bin\ffmpeg.exe -y -hide_banner -i "%~1" -filter "minterpolate='fps=120'" "%~n1_120fps%~x1"
+    ) else (
+        echo Invalid mode!
+    )
+
+    goto :end
+
+) else (
+    echo Please download FFmpeg and extract the bin folder to here.
+    echo Get it from https://ffmpeg.zeranoe.com/builds/
+    goto :end
+
+)
+
+:end
+echo.
+pause
